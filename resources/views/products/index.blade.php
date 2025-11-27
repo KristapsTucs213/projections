8cfa0cf9d41b669143f5d3b4363b1287f75fef6d
User <User>
2025-11-27 11:08:17 +0200

    tags balde

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -7,11 +7,24 @@
         @foreach ($products as $product)
             <li>
                 <h1>{{ $product->name }}</h1>
+
+                {{-- Show tags here --}}
+                @if ($product->tags->count())
+                    <div class="product-tags">
+                        <strong>Tags:</strong>
+                        @foreach ($product->tags as $tag)
+                            <span class="tag">{{ $tag->name }}</span>
+                        @endforeach
+                    </div>
+                @endif
+
                 <p>{{ $product->description }}</p>
+
                 <div class="product-actions">
-                    <a href="{{ route('products.show', [$product]) }}" class="btn btn-show">Show</a>
-                    <a href="{{ route('products.edit', [$product]) }}" class="btn btn-edit">Edit</a>
-                    <form action="{{ route('products.destroy', [$product]) }}" method="post">
+                    <a href="{{ route('products.show', $product) }}" class="btn btn-show">Show</a>
+                    <a href="{{ route('products.edit', $product) }}" class="btn btn-edit">Edit</a>
+
+                    <form action="{{ route('products.destroy', $product) }}" method="post">
                         @csrf
                         @method('DELETE')
                         <input type="submit" value="destroy" class="btn btn-delete">
